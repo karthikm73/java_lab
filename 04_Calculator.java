a6ba84bc358acd09c88fb5d7e98dbaa28ec98159
karthikm73 <121124166+karthikm73@users.noreply.github.com>
2023-07-18 21:12:22 +0530

    Update 04_Calculator.java

--- 04_Calculator.java
+++ 04_Calculator.java
@@ -1,82 +1,35 @@
 package javalab;
 
 import java.util.*;
-import java.lang.*;
+public class calc {
 
-public class Calculator
-{
-  Scanner sc= new Scanner(System.in);
-  public void add()
-  {
-	  System.out.println("Enter two numbers\n");
-	  int a=sc.nextInt();
-	  int b=sc.nextInt();
-	  System.out.println("The sum of "+a +" and " +b +" is "+(a+b));
-  }
-  
-  public void subtract()
-  {
-	  System.out.println("Enter two numbers\n");
-	  int a=sc.nextInt();
-	  int b=sc.nextInt();
-	  System.out.println("The difference between " +a +" and " +b +" is "+(a-b));
-  }
-  
-  public void divide()
-  {
-	  System.out.println("Enter two numbers\n");
-	  int a=sc.nextInt();
-	  int b=sc.nextInt();
-	  System.out.println(+a +" divided by " +b +" is "+(a/b));
-  }
-  
-  public void multiply()
-  {
-	  System.out.println("Enter two numbers\n");
-	  int a=sc.nextInt();
-	  int b=sc.nextInt();
-	  System.out.println("The product of "+a +" and " +b +" is "+(a*b));
-  }
-  
-  public void power()
-  {
-	  System.out.println("Enter two numbers\n");
-	  int a=sc.nextInt();
-	  int b=sc.nextInt();
-	  System.out.println(+a +" to the power of " +b +" is "+pow(a,b));
-  }
-  
-  public static void main(String[] args)
-  {
-	  
-	  System.out.println("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exit\n");
-	  Calculator c=new Calculator();
-	  while(true)
-	  {
-		  Scanner s= new Scanner(System.in);
-		  System.out.println("Enter your choice");
-		  int ch=s.nextInt();  
-		  switch(ch)
-		  {
-		  case  1:c.add();
-		          break;
-		          
-		  case  2:c.subtract();
-                  break;
-          
-		  case  3:c.multiply();
-                  break;
-          
-		  case  4:c.divide();
-                  break;
-          
-		  case  5:System.out.println("Exit from the program");
-		          System.exit(1); 
-          break;
- 
-          default:System.out.println("Wrong Choice.Try Again");
-          break;
-		  }
-	  }
-  }  
- }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int op1,op2;
+		char oper;
+		System.out.println("enter one operand");
+		op1=sc.nextInt();
+		System.out.println("enter operator + - * or /");
+		oper=sc.next().charAt(0);
+		System.out.println("enter another operand");
+			op2=sc.nextInt();
+			int res=0;
+			switch(oper)
+			{
+			case '+' : res= op1 + op2;
+			break;
+			case '-' : res= op1 - op2;
+			break;
+			case '*' : res= op1 * op2;
+			break;
+			case '/' : res= op1 / op2;
+			break;
+			default: System.out.println("invalid operator");
+			System.exit(0);
+			break;
+			}
+		System.out.println("result = "+ res);
+		
+	}
+
+}
